9f9632568b1173c10dd97da3d9f0f0e6756e6224
carolinasalimei <117692181+carolinasalimei@users.noreply.github.com>
2025-05-27 16:18:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+
+// Usá solamente process.env.PORT sin ningún número fijo
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
@@ -7,5 +9,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+  console.log(`Servidor escuchando en el puerto ${port}`);
 });
