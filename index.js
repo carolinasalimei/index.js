b47497765d6363faa69cb889d6ece37edd5de17f
carolinasalimei <117692181+carolinasalimei@users.noreply.github.com>
2025-05-27 19:27:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8080;
 
-// Usá solamente process.env.PORT sin ningún número fijo
-const port = process.env.PORT;
+app.use(express.json()); // para poder leer JSON en POST
+
+const rutasCompras = require('./routes/compras');
+app.use('/api/compras', rutasCompras);
 
 app.get('/', (req, res) => {
   res.send('Bienvenido a la API de Restoproyect');
